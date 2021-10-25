53ba4a5e99ba6fea7a78e6e58b26bbe189e8009d
Niklas Lampén <niklas@emptymonkey.com>
2021-10-25 23:06:17 +0300

    Update composer, fix code style

--- src/PhpCsFixer/RuleSet/EmptyMonkeySet.php
+++ src/PhpCsFixer/RuleSet/EmptyMonkeySet.php
@@ -1,30 +1,30 @@
 <?php
+
 declare(strict_types = 1);
 
 namespace EmptyMonkey\CodeStyle\PhpCsFixer\RuleSet;
 
 class EmptyMonkeySet extends \PhpCsFixer\RuleSet\AbstractRuleSetDescription
 {
-
     public function getDescription(): string
     {
         return 'Rule set as used by Empty Monkey development team, highly opinionated.';
     }
 
     /**
-     * @inheritDoc
+     * {@inheritDoc}
      */
     public function getRules(): array
     {
         return [
-            '@PSR1' => true,
-            '@PSR12' => true,
-            '@PSR2' => true,
-            '@PhpCsFixer' => true,
+            '@PSR1'                        => true,
+            '@PSR12'                       => true,
+            '@PSR2'                        => true,
+            '@PhpCsFixer'                  => true,
             'blank_line_after_opening_tag' => true,
-            'ordered_imports' => [
+            'ordered_imports'              => [
                 'sort_algorithm' => 'alpha',
-                'imports_order' => ['const', 'class', 'function'],
+                'imports_order'  => ['const', 'class', 'function'],
             ],
             'ordered_class_elements' => [
                 'order' => [
@@ -53,23 +53,23 @@ class EmptyMonkeySet extends \PhpCsFixer\RuleSet\AbstractRuleSetDescription
                 'sort_algorithm' => 'none',
             ],
             'no_superfluous_phpdoc_tags' => false,
-            'declare_equal_normalize' => [
+            'declare_equal_normalize'    => [
                 'space' => 'single',
             ],
-            'yoda_style' => false,
-            'increment_style' => true,
+            'yoda_style'             => false,
+            'increment_style'        => true,
             'binary_operator_spaces' => [
                 'default' => 'align_single_space_minimal',
             ],
             'assign_null_coalescing_to_coalesce_equal' => true,
-            'concat_space' => [
+            'concat_space'                             => [
                 'spacing' => 'one',
             ],
-            'return_assignment' => false,
-            'class_definition' => true,
+            'return_assignment'      => false,
+            'class_definition'       => true,
             'phpdoc_no_empty_return' => false,
-            'phpdoc_types_order' => [
-                'sort_algorithm' => 'none',
+            'phpdoc_types_order'     => [
+                'sort_algorithm'  => 'none',
                 'null_adjustment' => 'always_last',
             ],
             'return_type_declaration' => [
